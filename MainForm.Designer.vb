7d1fb7936b0f523f9b57a5ae46dd794540b621a5
manny-uncharted <omoebun52@gmail.com>
2022-07-22 22:05:10 +0100

    Add project files.

---
+++ MainForm.Designer.vb
@@ -0,0 +1,84 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Connect = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.Send = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Connect
+        '
+        Me.Connect.Location = New System.Drawing.Point(17, 12)
+        Me.Connect.Name = "Connect"
+        Me.Connect.Size = New System.Drawing.Size(247, 40)
+        Me.Connect.TabIndex = 0
+        Me.Connect.Text = "Connect"
+        Me.Connect.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(17, 69)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(247, 31)
+        Me.TextBox1.TabIndex = 1
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(17, 133)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(247, 250)
+        Me.RichTextBox1.TabIndex = 2
+        Me.RichTextBox1.Text = ""
+        '
+        'Send
+        '
+        Me.Send.Location = New System.Drawing.Point(286, 63)
+        Me.Send.Name = "Send"
+        Me.Send.Size = New System.Drawing.Size(187, 43)
+        Me.Send.TabIndex = 3
+        Me.Send.Text = "Send"
+        Me.Send.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Send)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Connect)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Connect As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents Send As Button
+End Class
---
+++ MainForm.vb
@@ -0,0 +1,64 @@
+﻿Imports System.Net
+Imports System.Net.Sockets
+Imports System.Text
+
+Public Class MainForm
+
+    Dim _client As TcpClient
+
+
+    Private Sub Connect_Click(sender As Object, e As EventArgs) Handles Connect.Click
+        Try
+            Dim ip As String = "127.0.0.1"
+            Dim port As Integer = 5432
+
+            _client = New TcpClient(ip, port)
+
+            CheckForIllegalCrossThreadCalls = False
+
+
+            Threading.ThreadPool.QueueUserWorkItem(AddressOf ReceiveMessages)
+
+            Me.AcceptButton = Send
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub ReceiveMessages(state As Object)
+        Try
+
+            While True
+
+                Dim ns As NetworkStream = _client.GetStream()
+                Dim toReceive(10000000) As Byte
+
+                ns.Read(toReceive, 0, toReceive.Length)
+                Dim txt As String = Encoding.ASCII.GetString(toReceive)
+
+                If RichTextBox1.TextLength > 0 Then
+                    RichTextBox1.Text &= vbCrLf & txt
+
+                Else
+                    RichTextBox1.Text = txt
+                End If
+
+            End While
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub Send_Click(sender As Object, e As EventArgs) Handles Send.Click
+        Try
+            Dim ns As NetworkStream = _client.GetStream()
+
+            ns.Write(Encoding.ASCII.GetBytes(TextBox1.Text), 0, TextBox1.Text.Length)
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+End Class
